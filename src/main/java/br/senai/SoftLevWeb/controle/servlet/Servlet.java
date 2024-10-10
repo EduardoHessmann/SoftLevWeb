bbe5784db878fd233d46ebbac56162e92129aa2d
EDUARDO HESSMANN ROHLEDER <eduardo_rohleder@estudante.sesisenai.org.br>
2024-10-10 10:32:46 -0300

    Criado a classe desnvolvedor

--- src/main/java/br/senai/SoftLevWeb/controle/servlet/Servlet.java
+++ src/main/java/br/senai/SoftLevWeb/controle/servlet/Servlet.java
@@ -1,5 +1,77 @@
 package br.senai.SoftLevWeb.controle.servlet;
 
-public class Servlet {
+import java.io.IOException;
+import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.senai.SoftLevWeb.modelo.dao.desenvolvedor.DesenvolvedorDAO;
+import br.senai.SoftLevWeb.modelo.dao.tarefa.TarefaDAO;
+import br.senai.SoftLevWeb.modelo.dao.tarefa.TarefaDAOImpl;
+import br.senai.SoftLevWeb.modelo.dao.tipoTarefa.TipoTarefaDAO;
+import br.senai.SoftLevWeb.modelo.dao.tipoTarefa.TipoTarefaDAOImpl;
+import br.senai.SoftLevWeb.modelo.dao.usuario.UsuarioDAO;
+import br.senai.SoftLevWeb.modelo.dao.usuario.UsuarioDAOImpl;
+
+@WebServlet("/")
+public class Servlet extends HttpServlet {
+
+	private static final long serialVersionUID = 8840029940617992062L;
+
+	private UsuarioDAO usuarioDAO;
+	private TarefaDAO tarefaDAO;
+	private TipoTarefaDAO tipoTarefaDAO;
+	private DesenvolvedorDAO desenvolvedorDAO;
+
+	public void init() {
+		usuarioDAO = new UsuarioDAOImpl();
+
+		tarefaDAO = new TarefaDAOImpl();
+		tipoTarefaDAO = new TipoTarefaDAOImpl();
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+
+		try {
+
+			switch (action) {
+
+			case "/index":
+				mostrarIndex(request, response);
+				break;
+
+			
+			default:
+				mostrarIndex(request, response);
+				break;
+			}
+
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+		
+		
+
+	}
+
+	private void mostrarIndex(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("assets/paginas/inedx.jsp");
+		dispatcher.forward(request, response);
+	}
+	
 }
--- src/main/java/br/senai/SoftLevWeb/modelo/entidade/desenvolvedor/Desenvolvedor.java
+++ src/main/java/br/senai/SoftLevWeb/modelo/entidade/desenvolvedor/Desenvolvedor.java
@@ -2,12 +2,80 @@ package br.senai.SoftLevWeb.modelo.entidade.desenvolvedor;
 
 import java.io.Serializable;
 
-import br.senai.SoftLevWeb.modelo.entidade.usuario.Usuario;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
-public class Desenvolvedor extends Usuario implements Serializable{
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "desenvolvedor")
+public class Desenvolvedor implements Serializable {
 
-	private static final long serialVersionUID = 7565692376369478802L;
+	private static final long serialVersionUID = 2482014914550908553L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_desenvolvedor")
+	private Long id;
+
+	@Column(name = "nome_desenvolvedor", length = 25, nullable = false)
+	private String nome;
+
+	@Column(name = "email_deselvolvedor", length = 65, nullable = false)
+	private String email;
+
+	@Column(name = "senha_desenvolvedor", length = 25, nullable = false)
+	private String senha;
 	
-	
+	public Desenvolvedor() {};
 
+	public Desenvolvedor(Long id, String nome, String email, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Desenvolvedor(String nome, String email, String senha) {
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
